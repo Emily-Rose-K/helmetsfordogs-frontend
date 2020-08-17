b6521a4f304b4ec22eb51f2e42259d037ca8873b
Emily Krauss <e.krauss27@gmail.com>
2020-08-17 12:54:57 -0700

    centered banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default function Banner() {
     return(
-        <h1>HELMETS FOR DOGS! 🐩 🐕</h1>
-
+        <Container fluid>
+            <Row>
+                <Col><h1 className="text-center">HELMETS FOR DOGS! 🐩 🐕</h1></Col>
+            </Row>
+        </Container>
     )
 }
