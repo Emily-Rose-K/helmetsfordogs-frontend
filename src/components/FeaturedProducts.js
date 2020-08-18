bd180949f6bc0d1e29cb1d24a69f06f68fe20a4b
Emily Krauss <e.krauss27@gmail.com>
2020-08-18 11:25:01 -0700

    shop page mostly laid out, needs functionality

--- src/components/FeaturedProducts.js
+++ src/components/FeaturedProducts.js
@@ -7,13 +7,13 @@ export default function FeaturedProducts() {
             <Row >
                 <Col> 
                     <Image className="m-5" src="/blue.png" alt="Minpin wearing a blue helmet." width="171" height="180" />
-                    <Image className="m-5" src="/blue.png" alt="Minpin wearing a blue helmet." width="171" height="180" /> 
+                    <Image className="m-5" src="/pink.jpg" alt="Minpin wearing a pink helmet." width="171" height="180" /> 
                 </Col>
             </Row>
             <Row >
                 <Col> 
-                    <Image className="m-5" src="/blue.png" alt="Minpin wearing a blue helmet." width="171" height="180" />
-                    <Image className="m-5"src="/blue.png" alt="Minpin wearing a blue helmet." width="171" height="180" /> 
+                    <Image className="m-5" src="/covid-helmet.jpg" alt="Minpin wearing a metal helmet." width="171" height="180" />
+                    <Image className="m-5"src="/ripley-cowboy.jpg" alt="Chorkie wearing a cowboy hat." width="171" height="180" /> 
                 </Col>
             </Row>
         </div>
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,7 +1,45 @@
 import React from 'react'
+import {Image, Container, Col,  Row} from 'react-bootstrap'
 
 export default function Shop() {
     return (
-        <h1> BUY A HELMET FOR UR DOG </h1>
+        <Container>
+            <Row>
+                <Col className="m-3" id="metal">
+                    <Image src="/metal.jpg" alt="metal helmet"  width="171" height="180" />
+                    <p>Metal helmet $7</p> <button>Add to cart🛒</button>
+                </Col> 
+                <Col className="m-3" id="pink">
+                    <Image src="/pink.jpg" alt="pink helmet"  width="171" height="180" />
+                    <p>Pink helmet $3</p> <button>Add to cart🛒</button>
+                </Col>
+                <Col className="m-3" id="blue">
+                    <Image src="/blue.png" alt="blue helmet"  width="171" height="180" />
+                    <p>Blue helmet $3</p> <button>Add to cart🛒</button>
+                </Col>
+                <Col className="m-3" id="cowgirl">
+                    <Image src="/ripley-cowboy.jpg" alt="cowboy hat" width="171" height="180" />
+                    <p>Cowgirl helmet $8</p> <button>Add to cart🛒</button>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="m-3" id="cowboy">
+                    <Image src="/giles-cowboy.jpg" alt="cowboy hat" width="171" height="180" />
+                    <p>Cowboy helmet $8</p> <button>Add to cart🛒</button>
+                </Col>
+                <Col className="m-3" id="grapefruit">
+                    <Image src="/grapefruit.jpg" alt=" grapefruit helmet" width="171" height="180" />
+                    <p>Grapefruit helmet $2</p> <button>Add to cart🛒</button>
+                </Col>
+                <Col className="m-3" id="covid">
+                    <Image src="/covid-helmet.jpg" alt="covid19 mask" width="171" height="180" />
+                    <p>Mask helmet $7</p> <button>Add to cart🛒</button>
+                </Col>
+                <Col className="m-3">
+                    <Image src="/bearhat.jpg" alt="bear hat" width="171" height="180" />
+                    <p>Bear helmet $12</p> <button>Add to cart🛒</button>
+                </Col>
+            </Row>
+        </Container>
     )
 }
